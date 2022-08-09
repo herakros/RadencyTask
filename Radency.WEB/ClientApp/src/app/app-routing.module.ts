c6df781c92e884dae578940fa71b458d8d09e96c
mayor.sheka <mayor.sheka@ukr.net>
2022-08-09 12:43:15 +0300

    added components and routes for them

--- Radency.WEB/ClientApp/src/app/app-routing.module.ts
+++ Radency.WEB/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListItemComponent } from './presentation/components/book-list-item/book-list-item.component';
+import { BookListComponent } from './presentation/components/book-list/book-list.component';
+import { BooksPageComponent } from './presentation/components/books-page/books-page.component';
+import { EditBookComponent } from './presentation/components/edit-book/edit-book.component';
+import { ViewBookComponent } from './presentation/components/view-book/view-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BooksPageComponent },
+  { path: 'book/list', component:BookListComponent },
+  { path: 'book/list/:id', component:BookListItemComponent },
+  { path: 'book/edit/:id', component:EditBookComponent },
+  { path: 'book/view/:id', component:ViewBookComponent },
+  { path: '**', component: BooksPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Radency.WEB/ClientApp/src/app/app.module.ts
+++ Radency.WEB/ClientApp/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BookListComponent } from './presentation/components/book-list/book-list.component';
+import { BookListItemComponent } from './presentation/components/book-list-item/book-list-item.component';
+import { BooksPageComponent } from './presentation/components/books-page/books-page.component';
+import { EditBookComponent } from './presentation/components/edit-book/edit-book.component';
+import { ViewBookComponent } from './presentation/components/view-book/view-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookListItemComponent,
+    BooksPageComponent,
+    EditBookComponent,
+    ViewBookComponent
   ],
   imports: [
     BrowserModule,
